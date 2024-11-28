4af6fb96fbc7c02e7f38c3bb8a4f3b6516d7b01a
Selvadharshini <selvadharshini12b@gmail.com>
2024-11-28 22:30:54 +0530

    Create Singly.java

---
+++ LinkedList/1.Creation of a Singly LinkedList/Singly.java
@@ -0,0 +1,8 @@
+public class Singly{
+    public static void main(String[] args) {
+        SinglyLL sll=new SinglyLL();
+        sll.createSinglyLinkedList(10);
+        System.out.println(sll.head.data);
+    }
+
+}
