36017d0677329f4b27d077fbf0aa77fe747f5063
Selvadharshini <selvadharshini12b@gmail.com>
2024-12-09 07:17:48 +0530

    Create BinaryTrees.java

---
+++ Trees/4. Post order Traversal/BinaryTrees.java
@@ -0,0 +1,43 @@
+public class BinaryTrees{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int index=-1;
+        public static Node buildTree(int []nodes){
+           index++;
+           if (index >= nodes.length || nodes[index] == -1) {
+            return null;
+        }
+           Node newnode=new Node(nodes[index]);
+           newnode.left=buildTree(nodes);
+           newnode.right=buildTree(nodes);
+           return newnode;
+        }
+    }
+    public static void postrdertraversal(Node root){ //left  -> right  -> root
+        if(root==null){
+            return;
+        }
+        
+        postrdertraversal(root.left);
+        
+        postrdertraversal(root.right);
+
+        System.out.print(root.data+" ");
+    }
+    public static void main(String[] args) {
+        int [] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(nodes); 
+        //System.out.print(root.data);
+        postordertraversal(root);
+    }
+}
