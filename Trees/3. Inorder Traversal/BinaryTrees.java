81ea36e2c7dd1bf29601a66de06df882e8cbd32f
Selvadharshini <selvadharshini12b@gmail.com>
2024-12-09 06:14:52 +0530

    Create BinaryTrees.java

---
+++ Trees/3. Inorder Traversal/BinaryTrees.java
@@ -0,0 +1,41 @@
+public class BinaryTrees{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int index=-1;
+        public static Node buildTree(int []nodes){
+           index++;
+           if (index >= nodes.length || nodes[index] == -1) {
+            return null;
+        }
+           Node newnode=new Node(nodes[index]);
+           newnode.left=buildTree(nodes);
+           newnode.right=buildTree(nodes);
+           return newnode;
+        }
+    }
+    public static void inordertraversal(Node root){ //left -> root -> right
+        if(root==null){
+            return;
+        }
+        
+        preordertraversal(root.left);
+        System.out.print(root.data+" ");
+        preordertraversal(root.right);
+    }
+    public static void main(String[] args) {
+        int [] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(nodes); 
+        //System.out.print(root.data);
+        inordertraversal(root);
+    }
+}
